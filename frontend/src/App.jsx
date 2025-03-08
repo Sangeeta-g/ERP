46e476e178bc32a6b8d1a23da49ac953becf1e04
Sanjana42796 <sanjanasanjana42796@gmail.com>
2025-03-08 11:06:07 +0530

    ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from './components/sidebar';
+import DashboardContent from './components/DashboardContent';
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app-container">
+      <Sidebar />
+      <div className="content-container">
+        <DashboardContent />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
