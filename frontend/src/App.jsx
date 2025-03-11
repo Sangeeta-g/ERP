8a46761d63f3aba10d6edec0d845d87a999b1dea
Sangeeta <sangeetagdani@gmail.com>
2025-03-11 10:10:08 +0530

    changes app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import UserForm from "./Components/UserForm";
 import DashboardContent from "./Components/DashboardContent"; // Corrected path
@@ -10,10 +9,14 @@ import Login from './Components/Login'; // Adjust the path as necessary
 import AdminPortal from './Components/AdminPortal'; // Create this component
 import HRPortal from './Components/HRPortal'; // Create this component
 import EmployeePortal from './Components/EmployeePortal'; // Create this component
-import UserForm from './Components/UserForm';
+import SalesManager from "./Components/SalesManager";
+import { UserProvider } from './UserContext'; 
+import ViewLeads from "./Components/ViewLeads";
+import AddLeads from "./Components/AddLeads";
 
 const App = () => {
     return (
+        <UserProvider>
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
@@ -21,12 +24,14 @@ const App = () => {
                 <Route path="/hr-portal" element={<HRPortal />} />
                 <Route path="/employee-portal" element={<EmployeePortal />} />
                 <Route path="/user-form" element={<UserForm />} /> {/* Corrected path & consistent naming */}
-                <Route path ="/dashboard-content" element={<DashboardContent /> }/> {/* Corrected path & consistent naming */}
-
+                <Route path ="/dashboard-content" element={<DashboardContent /> }/> 
+                <Route path="/sales-manager" element={<SalesManager />}/>
+                <Route path="/view-leads" element={<ViewLeads />}/>
+                <Route path="/add-leads" element={<AddLeads/>}/>
             </Routes>
         </Router>
-    );
+        </UserProvider>
+    );  
 };
 
 export default App;
-
